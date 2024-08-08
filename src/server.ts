ee945acb6e10f823e666c105d51bf396fe713439
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-08 11:28:57 -0300

    feat: add a route to create pessoas

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,30 @@ app.get('/', async (request: Request, response: Response) => {
   console.log(count)
 })
 
+app.post('/pessoas', async (request: Request, response: Response) => {
+  const nome = request.body['nome']
+  const nomeMae = request.body['nomeMae']
+  const nomePai = request.body['nomePai']
+  const cpf = request.body['cpf']
+  const dataNascimento = request.body['dataNascimento']
+  const salario = request.body['salario']
+  const observacoes = request.body['observacoes']
+
+  const pessoa = await db.pessoas.create({
+    data: {
+      nome,
+      nomeMae,
+      nomePai,
+      cpf,
+      dataNascimento,
+      salario,
+      observacoes
+    }
+  })
+
+  return response.status(201).json(pessoa)
+})
+
 const SERVER_PORT = 5000
 
 app.listen(SERVER_PORT, () => {
