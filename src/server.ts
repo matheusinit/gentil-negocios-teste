9135506f484b94f389f80f013abe9adf9fcd1b27
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-08 11:36:25 -0300

    feat: add a route to update pessoas

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,37 @@ app.post('/pessoas', async (request: Request, response: Response) => {
   return response.status(201).json(pessoa)
 })
 
+app.put('/pessoas/:id', async (request: Request, response: Response) => {
+  const id = request.params['id']
+
+  const nome = request.body['nome'] as string | undefined
+  const nomeMae = request.body['nomeMae'] as string | undefined
+  const nomePai = request.body['nomePai'] as string | undefined
+  const cpf = request.body['cpf'] as string | undefined
+  const dataNascimento = request.body['dataNascimento'] as string | undefined
+  const salario = request.body['salario'] as number | undefined
+  const observacoes = request.body['observacoes'] as string | undefined
+
+  const pessoa = await db.pessoas.update({
+    where: {
+      idPessoa: id
+    },
+    data: {
+      nome,
+      nomeMae,
+      nomePai,
+      cpf,
+      dataNascimento,
+      salario,
+      observacoes
+    }
+  })
+
+  return response.status(204).json(pessoa)
+})
+
+
+
 const SERVER_PORT = 5000
 
 app.listen(SERVER_PORT, () => {
