0f9bb3311437c9d7f81fa2c44b751e07d53a04b5
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-09 11:33:59 -0300

    feat: ensure when dataNascimento is not provided then should return bad request error

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,12 @@ app.post('/pessoas', async (request: Request, response: Response) => {
     })
   }
 
+  if (!dataNascimento) {
+    return response.status(400).send({
+      message: '\'dataNascimento\' is missing. Provide a valid string.'
+    })
+  }
+
   const dataNascimentoISOString = new Date(dataNascimento).toISOString()
 
   const pessoa = await db.pessoas.create({
