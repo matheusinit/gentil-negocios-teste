b508d12b43724107eab4f5ffd2438821ecdbbc53
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-09 11:25:21 -0300

    test: ensure when nome is not provided then should return bad request error

---
+++ tests/create-pessoas-controller.spec.ts
@@ -0,0 +1,60 @@
+import { it, describe, expect, beforeEach } from 'vitest'
+import request from 'supertest'
+import app from '../src/app'
+import { db } from '../src/infra/db'
+
+type Pessoa = {
+  nome?: string
+  nomeMae?: string
+  nomePai?: string
+  cpf?: string
+  dataNascimento?: String
+  salario?: number
+  observacoes?: string
+}
+
+describe('Create Pessoas Controller', () => {
+  beforeEach(async () => {
+    const tables = Object.keys(db).filter(model => !model.startsWith('_')).filter(model => !model.startsWith('$'));
+
+    for (const model of tables) {
+      try {
+        await db[model].deleteMany();
+        console.log(`Deleted all records from ${model}`);
+      } catch (error) {
+        console.error(`Failed to delete records from ${model}:`, error);
+      }
+    }
+
+    await db.$disconnect();
+  })
+
+  describe('given a invalid body', () => {
+    it('when \'nome\' is not provided, then should get bad request error', async () => {
+      //const pessoa: Pessoa = {
+      //  nome: "Matheus Fernandes",
+      //  nomeMae: "Ana Clara Marcedo Fernandes",
+      //  nomePai: "Mário José Marcedo",
+      //  cpf: "12345678900",
+      //  dataNascimento: "1990-03-21",
+      //  salario: 5000.00,
+      //  observacoes: "Funcionário do mês"
+      //}
+      const pessoa: Pessoa = {
+        nomeMae: "Ana Clara Marcedo Fernandes",
+        nomePai: "Mário José Marcedo",
+        cpf: "12345678900",
+        dataNascimento: "1990-03-21",
+        salario: 5000.00,
+        observacoes: "Funcionário do mês"
+      }
+
+      const response = await request(app).post('/pessoas').send(pessoa)
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body).toEqual({
+        message: '\'nome\' is missing. Provide a valid string.'
+      })
+    })
+  })
+})
