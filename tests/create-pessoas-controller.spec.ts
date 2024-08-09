a489e8bd37c3807d942a0566cfb1a96095197c25
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-09 11:32:36 -0300

    test: ensure when cpf is not provided then should return bad request error

--- tests/create-pessoas-controller.spec.ts
+++ tests/create-pessoas-controller.spec.ts
@@ -83,5 +83,23 @@ describe('Create Pessoas Controller', () => {
         message: '\'nomePai\' is missing. Provide a valid string.'
       })
     })
+
+    it('when \'cpf\' is not provided, then should get bad request error', async () => {
+      const pessoa: Pessoa = {
+        nome: "Matheus Fernandes",
+        nomeMae: "Ana Clara Marcedo Fernandes",
+        nomePai: "Mário José Marcedo",
+        dataNascimento: "1990-03-21",
+        salario: 5000.00,
+        observacoes: "Funcionário do mês"
+      }
+
+      const response = await request(app).post('/pessoas').send(pessoa)
+
+      expect(response.statusCode).toBe(400)
+      expect(response.body).toEqual({
+        message: '\'cpf\' is missing. Provide a valid string.'
+      })
+    })
   })
 })
